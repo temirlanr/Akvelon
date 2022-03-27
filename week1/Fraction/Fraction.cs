05adcedf0098f06f6d277fc1fe832810e1aa6f2d
temirlanr <rsanovtemirlan@gmail.com>
2022-03-27 15:17:40 +0600

    added Fraction

---
+++ week1/Fraction/Fraction.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    public class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            if(denominator == 0)
+            {
+                throw new ArgumentException("Denominator can't be 0");
+            }
+
+            int gcd = GCD(numerator, denominator);
+
+            this.numerator = numerator / gcd;
+            this.denominator = denominator / gcd;
+        }
+
+        public void Add(Fraction fraction)
+        {
+            if (denominator == fraction.denominator)
+            {
+                numerator += fraction.numerator;
+            }
+
+            numerator = numerator * fraction.denominator + fraction.numerator * denominator;
+            denominator *= fraction.denominator;
+        }
+
+        public void Subtract(Fraction fraction)
+        {
+            if (denominator == fraction.denominator)
+            {
+                numerator -= fraction.numerator;
+            }
+
+            numerator = numerator * fraction.denominator - fraction.numerator * denominator;
+            denominator *= fraction.denominator;
+        }
+
+        public void Multiply(Fraction fraction)
+        {
+            numerator *= fraction.numerator;
+            denominator *= fraction.denominator;
+        }
+
+        public void Divide(Fraction fraction)
+        {
+            numerator *= fraction.denominator;
+            denominator *= fraction.numerator;
+        }
+
+        public double ToDouble()
+        {
+            return numerator / denominator;
+        }
+
+        private static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+
+        private static int LCM(int a, int b)
+        {
+            return (a / GCD(a, b)) * b;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Fraction fraction &&
+                   numerator == fraction.numerator &&
+                   denominator == fraction.denominator;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(numerator, denominator);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{numerator}/{denominator}");
+        }
+    }
+}
--- week1/Fraction/Program.cs
+++ week1/Fraction/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Fraction
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var fraction1 = new Fraction(1, 4);
+            var fraction2 = new Fraction(2, 5);
+            var fraction3 = new Fraction(5, 75);
+            
+            Console.WriteLine(fraction2.Equals(new Fraction(2, 5)));
+
+            Console.WriteLine("fraction1 before subtraction:");
+            fraction1.Print();
+            fraction1.Subtract(fraction2);
+            Console.WriteLine("fraction1 after subtraction:");
+            fraction1.Print();
+
+            Console.WriteLine("fraction2 before multiplication:");
+            fraction2.Print();
+            fraction2.Multiply(fraction3);
+            Console.WriteLine("fraction2 after multiplication:");
+            fraction2.Print();
+        }
+    }
+}
