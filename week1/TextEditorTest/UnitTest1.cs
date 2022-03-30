23e19a2ec508fb78fefaa57de445e7076c64d57e
temirlanr <rsanovtemirlan@gmail.com>
2022-03-31 01:19:42 +0600

    renamed

---
+++ week1/TextEditorTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Text_Editor;
+using Xunit;
+
+namespace TextEditorTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void What_Tests_Can_Be_Done_Here_QUESTIONMARK()
+        {
+            
+        }
+    }
+}
