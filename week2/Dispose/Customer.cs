31384371c250e8384f84365162cd436c025387d5
temirlanr <rsanovtemirlan@gmail.com>
2022-04-02 23:54:25 +0600

    added week2(not finished)

---
+++ week2/Dispose/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispose
+{
+    public class Customer : IDisposable
+    {
+        private StringReader _reader;
+
+        public Customer(string s)
+        {
+            _reader = new StringReader(s);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ week2/Dispose/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dispose
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
