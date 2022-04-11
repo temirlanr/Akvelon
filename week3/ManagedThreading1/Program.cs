c584d847240fa6b6f3b9f6e60463f4c7c1830a45
temirlanr <rsanovtemirlan@gmail.com>
2022-04-11 15:41:20 +0600

    modified ManagedThreading1

--- week3/ManagedThreading1/Program.cs
+++ week3/ManagedThreading1/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading;
+
+namespace ManagedThreading1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread[] music = new Thread[3]
+            {
+                new Thread(Playlist) { Name = "Hardbass" },
+                new Thread(Playlist) { Name = "Latino" },
+                new Thread(Playlist) { Name = "Rock" }
+            };
+
+            for(int i = 0; i<30; i++)
+            {
+                var random = new Random();
+                var item = random.Next();
+            }
+
+            Thread[] dancers = new Thread[10];
+
+            for (int i = 0; i < dancers.Length; i++)
+            {
+                Thread dancer = new Thread(Dance) { Name = (i + 1).ToString() };
+                dancer.Start();
+            }
+        }
+
+        static void Dance()
+        {
+            if (Thread.CurrentThread.Name == "Hardbass")
+            {
+                Console.WriteLine("Elbow dance");
+            }
+
+            if(Thread.CurrentThread.Name == "Latino")
+            {
+                Console.WriteLine("Hips dance");
+            }
+
+            if(Thread.CurrentThread.Name == "Rock")
+            {
+                Console.WriteLine("Head dance");
+            }
+        }
+
+        static void Playlist()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} song");
+            Thread.Sleep(10000);
+        }
+    }
+}
