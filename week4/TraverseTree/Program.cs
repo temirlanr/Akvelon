fa39bb795010d522d2f0fca78ec728a5bb988dae
temirlanr <rsanovtemirlan@gmail.com>
2022-04-13 21:33:35 +0600

    added week4 folder

---
+++ week4/TraverseTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace TraverseTree
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ week4/TraverseTree/Tree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraverseTree
+{
+    public class Tree<T>
+    {
+        public Tree<T> Left;
+        public Tree<T> Right;
+        public T Data;
+
+        public static void DoTree<T>(Tree<T> tree, Action<T> action)
+        {
+            if (tree == null) return;
+            DoTree(tree.Left, action);
+            DoTree(tree.Right, action);
+            action(tree.Data);
+        }
+    }
+}
